8e5c4fb4825693028ecbb9f8e84bd1e8f2fc6b96
Tornike <itorn9777@gmail.com>
2023-04-08 00:43:34 +0400

    fix hero styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,10 @@
 
-import heroBg from '../../assets/hero.png'
 import cascade from '../../assets/cascade-img.png'
 import "./Hero.scss";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <img className='hero-bg' src={heroBg} />
       <div className="container flex-row">
         <div className="hero-wrapper flex-row">
           <h1>cascade</h1>
