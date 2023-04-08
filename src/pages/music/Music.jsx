63d8169ddc85b3443bd0d2a84b25aa37ec059d5b
Tornike <itorn9777@gmail.com>
2023-04-08 19:01:43 +0400

    add bg to music page

--- src/pages/music/Music.jsx
+++ src/pages/music/Music.jsx
@@ -5,19 +5,23 @@ import Button from '../../components/button/Button'
 
 import { popular, podcast, albumPreview, epPreview } from '../../data'
 
+import './Music.scss'
+
 const Music = () => {
   return (
-    <div style={{color: 'white'}}>
+    <div className='music' style={{color: 'white'}}>
       <PageTitle page='music' />
       <div className="container">
-        <Row data={popular} title="popular" />
-        <div className="preview-container flex-row" style={{justifyContent: 'flex-start', gap: 80, margin: '150px 0'}}>
-          <Row data={albumPreview} title="album review" />
-          <Row data={epPreview} title="ep review" />
-        </div>
-        <Row data={podcast} title="podcast series" />
-        <div className='flex-row' style={{margin: '60px 0'}}>
-          <Button />
+        <div className="music-wrapper">
+          <Row data={popular} title="popular" />
+          <div className="preview-container flex-row" style={{justifyContent: 'flex-start', gap: 80, margin: '150px 0'}}>
+            <Row data={albumPreview} title="album review" />
+            <Row data={epPreview} title="ep review" />
+          </div>
+          <Row data={podcast} title="podcast series" />
+          <div className='flex-row' style={{margin: '60px 0'}}>
+            <Button />
+          </div>
         </div>
       </div>
     </div>
