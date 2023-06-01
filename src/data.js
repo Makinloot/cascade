dbed1d3236193c8dcd8f2918e597472ec2ff975e
Tornike <itorn9777@gmail.com>
2023-06-01 14:44:02 +0400

    add new podcast

--- src/data.js
+++ src/data.js
@@ -17,6 +17,9 @@ import slideImg8 from './assets/slider/slide8.png'
 import slideImg9 from './assets/slider/slide9.png'
 import slideImg10 from './assets/slider/slide10.png'
 import slideImg11 from './assets/slider/slide11.png'
+import slideImg12 from './assets/slider/slide12.png'
+
+const randomId = () => Math.random() * Math.random() * Math.random();
 
 export const popular = [
   {
@@ -76,67 +79,73 @@ export const epPreview = [
 
 export const sliderData = [
   {
-    id: 1,
+    id: randomId(),
+    cover: slideImg12,
+    title: 'Selector Series 15',
+    link: 'https://soundcloud.com/ramzi911/selector-series-15?si=1003917bf1764061a152415596a65c7f&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+  },
+  {
+    id: randomId(),
     cover: slideImg,
     title: 'ACT',
     link: 'https://soundcloud.com/ramzi911/sets/act?si=43e714cc5b854ee599a7b50cdebe99cc&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
   },
   {
-    id: 2,
+    id: randomId(),
     cover: slideImg2,
     title: 'Techno Revelation Part 3',
     link: 'https://soundcloud.com/ramzi911/techno-revelation-part-3?si=c965635502604819962250c5d422d2c8&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 3,
+    id: randomId(),
     cover: slideImg3,
     title: 'Ramzi X IV - Barrier Y .mp3',
     link: 'https://soundcloud.com/ivempire/ramzi-x-iv-barrier-y-mp3?si=c93daa3c504149d0921ea1c150c04c79&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 4,
+    id: randomId(),
     cover: slideImg4,
     title: 'Cascade Selector Series 12 By Ramzi',
     link: 'https://soundcloud.com/ramzi911/cascade-sellector-series-12-by-ramzi?si=a1b3c96927f049fb8a0fad6c9737b09a&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 5,
+    id: randomId(),
     cover: slideImg5,
     title: 'Cascade selector resies: chapter 6 mixed by Ramzi',
     link: 'https://soundcloud.com/ramzi911/cascade-selector-resies-chapter-6-mixed-by-ramzi?si=4f7d6d9b5a16442aa4d7b71a9a1105e7&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 6,
+    id: randomId(),
     cover: slideImg6,
     title: 'Cascade selector series: chapter 5 mixed by Ramzi B2B iv',
     link: 'https://soundcloud.com/cascade-records/cascade-selector-series-chapter-5-mixed-by-ramzi-b2b-iv?si=f239fba3110540a09ca4a331d715c41e&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 7,
+    id: randomId(),
     cover: slideImg7,
     title: 'Cascade selector series: chapter 2 mixed by Ramzi B2B iv',
     link: 'https://soundcloud.com/cascade-records/cascade-selector-series-chapter-2-mixed-by-ramzi-b2b-iv?si=9ce61cf916a94e6b81b5452d8a3bd12e&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 8,
+    id: randomId(),
     cover: slideImg8,
     title: 'Techno Revelations Part 1',
     link: 'https://soundcloud.com/ramzi911/techno-revelations-part-1?si=0ac7003a85034681917d2785df2152f0&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 9,
+    id: randomId(),
     cover: slideImg9,
     title: 'Techno Act 1 Mixed By Ramzi',
     link: 'https://soundcloud.com/ramzi911/techno-act-1-mixed-by-ramzi?si=1aa3ffda0f8a4d5bb9889257b8ea6700&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 10,
+    id: randomId(),
     cover: slideImg10,
     title: 'Cascade selector series: chapter 3 mixed by Ramzi',
     link: 'https://soundcloud.com/ramzi911/cascade-selector-series-chapter-3-mixed-by-ramzi-b2b-iv?si=bd81b9b486a74c2c95f6f4c5984b35f5&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
   },
   {
-    id: 11,
+    id: randomId(),
     cover: slideImg11,
     title: 'Cascade selector series: chapter 1 mixed By Ramzi B2B iv',
     link: 'https://soundcloud.com/ramzi911/cascade-selector-series-chapter-1-mixed-by-ramzi-b2b-iv?si=58b1983f2a544b798f31ecddfdb57d83&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
